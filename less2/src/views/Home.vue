0a7c2b9c254075a38917fd2e105228fbb4123758
mihail1812 <vahturov.m@yandex.ru>
2021-10-19 01:38:06 +0400

    less2 new project

--- less2/src/views/Home.vue
+++ less2/src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
+    <HelloWorld msg="ДЗ урока2 по ссылке Less2"/>
   </div>
 </template>
 
